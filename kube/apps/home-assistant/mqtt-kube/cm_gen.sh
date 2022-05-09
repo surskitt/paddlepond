3951eaea28952cd171a1105d70e6d8b926833156
shanedabes <shane@isda.best>
2022-05-09 22:21:54 +0100

    finish adding media deployments to mqtt-kube

---
+++ kube/apps/home-assistant/mqtt-kube/cm_gen.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$(dirname ${0})"
+
+cat << EOF > "${SCRIPT_DIR}/configmap.yaml"
+---
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: mqtt-kube-config
+  namespace: home-assistant
+data:
+  config.yaml: |
+    mqtt:
+      host: mosquitto
+      port: 1883
+      client-id: mqtt-kube
+      topic-prefix: mqtt-kube
+
+    http:
+      host: 0.0.0.0
+      port: 8080
+      max-connections: 10
+
+    logging:
+      level: DEBUG
+
+    bindings:
+EOF
+
+for deploy in bazarr calibre-web flood jackett komga mylar nzbget plex pyload radarr sonarr transmission; do
+    cat << EOF >> "${SCRIPT_DIR}/configmap.yaml"
+      - namespace: media
+        resource: Deployment
+        name: ${deploy}
+
+        patch:
+          - topic: ${deploy}/power/set
+            locus: "spec.replicas"
+            values:
+              map:
+                "ON": 1
+                "OFF": 0
+
+        watch:
+          - locus: "status.availableReplicas"
+            topic: ${deploy}/power/state
+            retain: true
+            qos: 1
+            values:
+              map:
+                1: "ON"
+                ~: "OFF"
+
+EOF
+done
