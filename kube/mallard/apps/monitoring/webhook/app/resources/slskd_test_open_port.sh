5fe50d93eaca085703565bcf1b0a60a8a7052691
surskitt <surskitt@users.noreply.github.com>
2025-12-26 02:45:13 +0000

    fix(webhook): add redirect to gluetun api curl

--- kube/mallard/apps/monitoring/webhook/app/resources/slskd_test_open_port.sh
+++ kube/mallard/apps/monitoring/webhook/app/resources/slskd_test_open_port.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-public_ip="$(curl -s http://slskd.media.svc.cluster.local:8000/v1/publicip/ip | jq -r .public_ip)"
-listen_port="$(curl -s http://slskd.media.svc.cluster.local:8000/v1/openvpn/portforwarded | jq .port)"
+public_ip="$(curl -L -s http://slskd.media.svc.cluster.local:8000/v1/publicip/ip | jq -r .public_ip)"
+listen_port="$(curl -L -s http://slskd.media.svc.cluster.local:8000/v1/openvpn/portforwarded | jq .port)"
 
 nc -w 5 -vz "${public_ip}" "${listen_port}"
